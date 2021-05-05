381c52e1fd95a7574398efcbe95e62abbae67959
Yuunan Kin <yuunan.kin@gmail.com>
2021-05-05 09:27:00 +0900

    remove unneeded redux types

--- src/app/actions/types.ts
+++ src/app/actions/types.ts
@@ -9,10 +9,3 @@ export const AUTH_EXPIRED = "AUTH_EXPIRED";
 // message
 export const SHOW_MESSAGES = "SHOW_MESSAGES";
 export const CLEAR_MESSAGES = "CLEAR_MESSAGES";
-// meeting
-export const UPDATE_USERS = "UPDATE_USERS";
-export const ADD_AUDIO = "ADD_AUDIO";
-export const REMOVE_AUDIO = "REMOVE_AUDIO";
-export const CLEAR_AUDIO = "CLEAR_AUDIO";
-export const AUDIO_OUTPUT_DEVICE = "AUDIO_OUTPUT_DEVICE";
-export const AUDIO_INPUT_DEVICE = "AUDIO_INPUT_DEVICE";
